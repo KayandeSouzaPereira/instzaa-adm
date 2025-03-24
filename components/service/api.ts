6df630607177d2c4a6784de63da38d2c762b03b7
Kayan de Souza <40444457+KayandeSouzaPereira@users.noreply.github.com>
2025-03-24 11:49:24 -0300

    Update api.ts

--- components/service/api.ts
+++ components/service/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://ec2-56-124-45-234.sa-east-1.compute.amazonaws.com:8080/",
+  baseURL: "http://ec2-56-124-45-234.sa-east-1.compute.amazonaws.com/",
   //baseURL: "http://localhost:8080/",
 });
 
